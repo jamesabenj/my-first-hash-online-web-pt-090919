84020e69ad085f584b0837a89caf645806668795
James Benjamin <james.abenj@gmail.com>
2019-09-21 17:58:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,10 +22,10 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest["oil paintings"]
-  
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
+    # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -36,12 +36,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"] = 2 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
